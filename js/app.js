0f767c565123f90ab4bd5b4509941db545c23daf
treyh23 <forrest@contempows.com>
2018-01-29 13:15:26 -0800

    compeleted date function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var getForm = document.getElementById('nameDate');
-var currentDate = new Date();
+var drinkingDate = new Date();
+var yearsAgo = drinkingDate.getFullYear() - 21;
+drinkingDate.setFullYear(yearsAgo);
 
 
 function handleClick(event) {
@@ -9,8 +11,19 @@ function handleClick(event) {
   var userName = event.target.name.value;
   var birthDate = event.target.userBirthDate.value;
   localStorage.setItem('userName',userName);
+  determineDrinkingAge(new Date(birthDate + 'PST'));
+  // debugger;
+  console.log(typeof birthDate);
 }
 
+function determineDrinkingAge(birthDate) {
+  if(birthDate <= drinkingDate) {
+    console.log('over 21');
+  } else {
+    console.log('No alc for you');
+  }
+  
+}
 
 getForm.addEventListener('submit', handleClick);
 
