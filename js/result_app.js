0ea28d27bc90777ddbf5b53e0179e28e543516dd
treyh23 <forrest@contempows.com>
2018-01-30 14:54:18 -0800

    new object

--- js/result_app.js
+++ js/result_app.js
@@ -3,7 +3,34 @@
 // global variables
 var fAnswers = localStorage.answers.split(',');
 // get results from local storage
-JSON.parse(localStorage.getItem('answer'))
+
+
+var questions = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7,
+  yes: 2,
+  no: 1,
+  0: 1,
+  1: 2,
+  2: 3,
+  3: 4,
+  4: 5,
+  5: 6,
+  6: 7,
+  rich: 1,
+  stable: 2,
+  p2p: 3,
+  maxcc: 4,
+  collegeL: 5,
+  billPast: 6,
+  carRepoNFor: 7,
+}
+
 // functions
 
 // function call to render page
